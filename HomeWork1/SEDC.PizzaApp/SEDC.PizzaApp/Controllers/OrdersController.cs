397a44aa7ce143ba0319f9f9446be70bf5a3cd54
Stefan <75165862+TrajkovStefan@users.noreply.github.com>
2021-08-09 13:47:32 +0200

    HomeWork1

---
+++ HomeWork1/SEDC.PizzaApp/SEDC.PizzaApp/Controllers/OrdersController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.Controllers
+{
+    public class OrdersController : Controller
+    {
+        //Add Index action that returns a view with a simple html that says “List of orders”. It should be accessed through custom route
+        [Route("Orders")]
+        public IActionResult Index()
+        {
+            List<Order> orders = StaticDb.Orders;
+            return View(orders);
+        }
+        [Route("OrdersController/Details/{id?}")]
+        public IActionResult Details(int? id)
+        {
+            if(id == 0)
+            {
+                return new EmptyResult();
+            }
+            Order order = StaticDb.Orders.FirstOrDefault(x => x.Id == id);
+            if(order == null)
+            {
+                return new EmptyResult();
+            }
+            return View(order);
+        }
+        [Route("OrdersController/JsonData")]
+        public IActionResult JsonReturns()
+        {
+            return new JsonResult(StaticDb.Orders);
+        }
+        public IActionResult RedirectToHome()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ HomeWork1/SEDC.PizzaApp/SEDC.PizzaApp/Controllers/PizzaController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Pizza> pizzas = StaticDb.Pizzas;
+            return View(pizzas);
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new EmptyResult();
+            }
+            Pizza pizza = StaticDb.Pizzas.FirstOrDefault(x => x.Id == id);
+            if (pizza == null)
+            {
+                return new EmptyResult();
+            }
+            return View(pizza);
+        }
+
+        [Route("SeePizzas")]
+        public IActionResult Redirect()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
