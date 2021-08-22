cda25ea2f8301b27dd0ebb9a271af82522866060
Stefan <75165862+TrajkovStefan@users.noreply.github.com>
2021-08-23 00:50:17 +0200

    HomeWork4

---
+++ HomeWork4/SEDC.Pizza.App.Refactored/SEDC.PizzaApp.Domain/Models/Pizza.cs
@@ -0,0 +1,16 @@
+﻿using SEDC.PizzaApp.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.PizzaApp.Domain.Models
+{
+    public class Pizza : BaseEntity
+    {
+        public string Name { get; set; }
+        public PizzaSizeEnum PizzaSize { get; set; }
+        public int Price { get; set; }
+        public bool IsOnPromotion { get; set; }
+        public List<PizzaOrder> PizzaOrders { get; set; }
+    }
+}
---
+++ HomeWork4/SEDC.Pizza.App.Refactored/SEDC.PizzaApp.Mappers/PizzaMapper.cs
@@ -0,0 +1,47 @@
+﻿using SEDC.PizzaApp.Domain.Models;
+using SEDC.PizzaApp.ViewModels.PizzaViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.PizzaApp.Mappers
+{
+    public static class PizzaMapper
+    {
+        public static PizzaListViewModel PizzaToPizzaListViewModel(Pizza pizza)
+        {
+            return new PizzaListViewModel
+            {
+                Id = pizza.Id,
+                Name = pizza.Name,
+                PizzaSize = pizza.PizzaSize,
+                Price = pizza.Price,
+                IsOnPromotion = pizza.IsOnPromotion
+            };
+        }
+
+        public static PizzaDetailsViewModel PizzaDetailsViewModel(this Pizza pizza)
+        {
+            return new PizzaDetailsViewModel
+            {
+                Id = pizza.Id,
+                Name = pizza.Name,
+                PizzaSize = pizza.PizzaSize,
+                Price = pizza.Price,
+                IsOnPromotion = pizza.IsOnPromotion
+            };
+        }
+
+        public static Pizza ToPizza(this PizzaViewModel pizzaViewModel)
+        {
+            return new Pizza
+            {
+                Id = pizzaViewModel.Id,
+                Name = pizzaViewModel.Name,
+                PizzaSize = pizzaViewModel.PizzaSize,
+                Price = pizzaViewModel.Price,
+                IsOnPromotion = pizzaViewModel.IsOnPromotion
+            };
+        }
+    }
+}
